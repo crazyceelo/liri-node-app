cb04ff8f23db613dbb66c3ede3e71a3c2f1653b1
Carlos Perez <solracias@gmail.com>
2017-05-21 09:03:10 -0700

    added npm packages.created twitter variables and .gitignore file

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here.',
-    access_token_secret: "<input here>"
+    consumer_key: 'Qcst4zcukIvkzsaZQaCfgrNIW',
+    consumer_secret: 'zEfT1J7ATCKYnZqOpfE9BHlgPDFVs2OcouoHbvIU8E6SAeysfm',
+    access_token_key: '866315525927849984-ryRXznD9JEEsv0FL38hjKO5dou6s6d5',
+    access_token_secret: "TK7HImU2IdrhkWgP92d3hdpsQqiPNd9EGR9Q6hhrK9UJ0"
 }
---
+++ liri.js
@@ -0,0 +1,6 @@
+var twitter = require("./keys.js");
+
+var twitterToken = twitter.twitterKeys.access_token_key;
+var twitterTokenSecret = twitter.twitterKeys.access_token_secret;
+var twitterConsumerKey = twitter.twitterKeys.consumer_key;
+var twitterConsumerSecret = twitter.twitterKeys.consumer_secret;
