1e9d168a8c1f5cea08a01ec3da16635c3858fc98
Carlos Perez <solracias@gmail.com>
2017-05-21 11:05:44 -0700

    can now query for twitter

--- liri.js
+++ liri.js
@@ -1,18 +1,39 @@
-var twitter = require("./keys.js");
+var keys = require("./keys.js");
+var Twitter = require('twitter');
 
-var client = ({
-    twitterToken : twitter.twitterKeys.access_token_key,
-    twitterTokenSecret : twitter.twitterKeys.access_token_secret,
-    twitterConsumerKey : twitter.twitterKeys.consumer_key,
-    twitterConsumerSecret : twitter.twitterKeys.consumer_secret
+var client = new Twitter({
+    consumer_key: "Qcst4zcukIvkzsaZQaCfgrNIW",
+    consumer_secret: "zEfT1J7ATCKYnZqOpfE9BHlgPDFVs2OcouoHbvIU8E6SAeysfm",
+    access_token_key: "866315525927849984-ryRXznD9JEEsv0FL38hjKO5dou6s6d5",
+    access_token_secret: "TK7HImU2IdrhkWgP92d3hdpsQqiPNd9EGR9Q6hhrK9UJ0"
 })
 
-// console.log(client.twitterToken);
-// console.log(client.twitterTokenSecret);
-// console.log(client.twitterConsumerKey);
-// console.log(client.twitterConsumerSecret);
+var key = ({
+    twitterToken : keys.twitterKeys.access_token_key,
+    twitterTokenSecret : keys.twitterKeys.access_token_secret,
+    twitterConsumerKey : keys.twitterKeys.consumer_key,
+    twitterConsumerSecret : keys.twitterKeys.consumer_secret
+})
+
+console.log(key.twitterToken);
+console.log(key.twitterTokenSecret);
+console.log(key.twitterConsumerKey);
+console.log(key.twitterConsumerSecret);
 
 var myTweets = process.argv[2];
 var spotifyThisSong = process.argv[3];
 var movieThis = process.argv[4];
 var doWhatItSays = process.argv[5];
+
+console.log(myTweets);
+console.log(spotifyThisSong);
+console.log(movieThis);
+console.log(doWhatItSays);
+
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function (error, tweets, response){
+    if (!error) {
+        console.log(tweets);
+    }
+})
