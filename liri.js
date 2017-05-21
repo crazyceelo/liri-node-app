7a16167dce1804e797af784375eaa7252feff0b4
Carlos Perez <solracias@gmail.com>
2017-05-21 10:31:12 -0700

    added input variables

--- liri.js
+++ liri.js
@@ -1,6 +1,18 @@
 var twitter = require("./keys.js");
 
-var twitterToken = twitter.twitterKeys.access_token_key;
-var twitterTokenSecret = twitter.twitterKeys.access_token_secret;
-var twitterConsumerKey = twitter.twitterKeys.consumer_key;
-var twitterConsumerSecret = twitter.twitterKeys.consumer_secret;
+var client = ({
+    twitterToken : twitter.twitterKeys.access_token_key,
+    twitterTokenSecret : twitter.twitterKeys.access_token_secret,
+    twitterConsumerKey : twitter.twitterKeys.consumer_key,
+    twitterConsumerSecret : twitter.twitterKeys.consumer_secret
+})
+
+// console.log(client.twitterToken);
+// console.log(client.twitterTokenSecret);
+// console.log(client.twitterConsumerKey);
+// console.log(client.twitterConsumerSecret);
+
+var myTweets = process.argv[2];
+var spotifyThisSong = process.argv[3];
+var movieThis = process.argv[4];
+var doWhatItSays = process.argv[5];
