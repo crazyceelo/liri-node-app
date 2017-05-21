10a2e29529f4b64aea250ca55f2ab25fd075b0b3
Carlos Perez <solracias@gmail.com>
2017-05-21 14:10:35 -0700

    now sorts by song

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var spotify = require('spotify');
 
 var client = new Twitter({
     consumer_key: "Qcst4zcukIvkzsaZQaCfgrNIW",
@@ -15,25 +16,55 @@ var key = ({
     twitterConsumerSecret : keys.twitterKeys.consumer_secret
 })
 
-console.log(key.twitterToken);
-console.log(key.twitterTokenSecret);
-console.log(key.twitterConsumerKey);
-console.log(key.twitterConsumerSecret);
+// console.log(key.twitterToken);
+// console.log(key.twitterTokenSecret);
+// console.log(key.twitterConsumerKey);
+// console.log(key.twitterConsumerSecret);
 
-var myTweets = process.argv[2];
-var spotifyThisSong = process.argv[3];
-var movieThis = process.argv[4];
-var doWhatItSays = process.argv[5];
+var input = process.argv[2];
+var input2 = process.argv[3];
 
-console.log(myTweets);
-console.log(spotifyThisSong);
-console.log(movieThis);
-console.log(doWhatItSays);
+// var myTweets = process.argv[2];
+// var spotifyThisSong = process.argv[3];
+// var movieThis = process.argv[4];
+// var doWhatItSays = process.argv[5];
+
+// console.log(myTweets);
+// console.log(spotifyThisSong);
+// console.log(movieThis);
+// console.log(doWhatItSays);
 
 
-var params = {screen_name: 'nodejs'};
+var params = {solracias: 'nodejs'};
 client.get('statuses/user_timeline', params, function (error, tweets, response){
-    if (!error) {
-        console.log(JSON.stringify(tweets, null, 2));
+    if (input === "my-tweets") {
+        // console.log(tweets);
+        // console.log(JSON.stringify(tweets, null, 2));
+
+        for (var i = 0; i < tweets.length; i++){
+            console.log("Tweet "+ [i + 1] + ": "+ "(" + tweets[i].created_at + ") " + tweets[i].text);
+        }
+    }
+
+    else {
+        console.log(error, "-->" + " Please type <my-tweets> to get the last 20 tweets");
+    }
+})
+
+spotify.search({type: 'track', query: input2}, function(err, data){
+    if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+    else if (!err && input == "spotify-this-song") {
+        for (var i = 0; i < data.tracks.items.length; i++){
+            console.log("");
+            console.log("Album: " + data.tracks.items[i].album.name);
+            console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+            console.log("song name: " + data.tracks.items[i].name);
+            console.log(data.tracks.items[i].preview_url);
+            console.log("");
+        }
     }
 })
