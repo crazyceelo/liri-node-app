3f20bb29a52e70e9853ea287aa23d39bd7080703
Carlos Perez <solracias@gmail.com>
2017-05-21 18:17:38 -0700

    can now read file, but can't write yet

--- liri.js
+++ liri.js
@@ -45,67 +45,64 @@ var input2 = process.argv[3];
 
 var params = {solracias: 'nodejs'};
 client.get('statuses/user_timeline', params, function (error, tweets, response){
-    if (input === "my-tweets") {
-        // console.log(tweets);
-        // console.log(JSON.stringify(tweets, null, 2));
-        console.log("-----------")
-        console.log("results")
-        console.log("-----------")
+        if (input === "my-tweets") {
+            // console.log(tweets);
+            // console.log(JSON.stringify(tweets, null, 2));
+            console.log("-----------")
+            console.log("results")
+            console.log("-----------")
 
-        for (var i = 0; i < tweets.length; i++){
-            console.log("Tweet "+ [i + 1] + ": "+ "(" + tweets[i].created_at + ") " + tweets[i].text);
-        }
-    }
-
-    // else {
-    //     console.log(error, "-->" + " Please type <my-tweets> to get the last 20 tweets");
-    // }
-})
-
-spotify.search({type: 'track', query: input2}, function(err, data){
-    if (err) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-
-    else if (input2 == null && !err && input === "spotify-this-song"){
-        spotify.search({type: 'track', query: "The Sign"}, function(err, data){
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
+            for (var i = 0; i < tweets.length; i++){
+                console.log("Tweet "+ [i + 1] + ": "+ "(" + tweets[i].created_at + ") " + tweets[i].text);
             }
-            console.log("-----------")
-            console.log("results")
-            console.log("-----------")
-            // console.log(JSON.stringify(data, null, 2));
-            console.log("Album: " + data.tracks.items[4].album.name);
-            console.log("Artist: " + data.tracks.items[4].album.artists[0].name);
-            console.log("Song name: " + data.tracks.items[4].name);
-            console.log("Preview URL: " + data.tracks.items[4].preview_url)
-            console.log("");
-        })
-    }
-
-    else if (!err && input == "spotify-this-song") {
-        for (var i = 0; i < data.tracks.items.length; i++){
-            console.log("-----------")
-            console.log("results")
-            console.log("-----------")
-            console.log("Album: " + data.tracks.items[i].album.name);
-            console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
-            console.log("song name: " + data.tracks.items[i].name);
-            console.log("Preview URL: " + data.tracks.items[i].preview_url);
-            console.log("");
         }
-    }
 
-    // I might want to use this if I want this to show up when all inputs are null
-    // else if (!err && input == null && input2 == null){
-    //     console.log("test");
-    // }
+        // else {
+        //     console.log(error, "-->" + " Please type <my-tweets> to get the last 20 tweets");
+        // }
+    })
 
-    
-})
+function spot(input2){
+        spotify.search({type: 'track', query: input2}, function(err, data){
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        else if (input2 == null && !err && input === "spotify-this-song"){
+            spotify.search({type: 'track', query: "The Sign"}, function(err, data){
+                if (err) {
+                    console.log('Error occurred: ' + err);
+                    return;
+                }
+                console.log("-----------")
+                console.log("results")
+                console.log("-----------")
+                // console.log(JSON.stringify(data, null, 2));
+                console.log("Album: " + data.tracks.items[4].album.name);
+                console.log("Artist: " + data.tracks.items[4].album.artists[0].name);
+                console.log("Song name: " + data.tracks.items[4].name);
+                console.log("Preview URL: " + data.tracks.items[4].preview_url)
+                console.log("");
+            })
+        }
+
+        else if (!err && input == "spotify-this-song") {
+            for (var i = 0; i < data.tracks.items.length; i++){
+                console.log("-----------")
+                console.log("results")
+                console.log("-----------")
+                console.log("Album: " + data.tracks.items[i].album.name);
+                console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+                console.log("song name: " + data.tracks.items[i].name);
+                console.log("Preview URL: " + data.tracks.items[i].preview_url);
+                console.log("");
+            }
+        }  
+    })
+}
+spot(input2);
+
 
 
 request('http://www.omdbapi.com/?i=tt3896198&apikey=23fb74b2&t='+ input2 +'', function(error, response, body){
@@ -143,17 +140,62 @@ request('http://www.omdbapi.com/?i=tt3896198&apikey=23fb74b2&t='+ input2 +'', fu
     }
 })
 
-var a = "";
-var b = "";
 
 fs.readFile('./random.txt', 'utf8', function(error, data){
     if (error){
         return console.log(error);
     }
+
     var split = data.split(",");
     for(var i = 0; i < split.length; i++){
         // console.log(split[i]);
     }
-    a = split[0];
-    b = split[1];
-}).then
+    var a = split[0];
+    var b = split[1];
+    console.log(a);
+    console.log(b);
+
+    // spotify.search({type: 'track', query: b}, function (err, data){
+    //     // console.log(JSON.stringify(data, null, 2));
+    //     console.log("-----------")
+    //     console.log("results")
+    //     console.log("-----------")
+    //     // console.log(JSON.stringify(data, null, 2));
+    //     console.log("Album: " + data.tracks.items[0].album.name);
+    //     console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+    //     console.log("Song name: " + data.tracks.items[0].name);
+    //     console.log("Preview URL: " + data.tracks.items[0].preview_url)
+    //     console.log("");
+    //     })
+    // }
+});
+// spotify.search({type: 'track', query: input2}, function(err, data){
+//         if (err) {
+//             console.log('Error occurred: ' + err);
+//             return;
+//         }
+
+// var geocoder = require("geocoder");
+// var inquirer = require("inquirer");
+
+// // Prompt the user to provide location information.
+// inquirer.prompt([
+
+//   {
+//     type: "input",
+//     name: "userInput",
+//     message: "Which location or landmark would you like to geocode?"
+//   }
+
+// // After the prompt, store the user's response in a variable called location.
+// ]).then(function(location) {
+
+//   // console.log(location.userInput);
+
+//   // Then use the Google Geocoder to Geocode the address
+//   geocoder.geocode(location.userInput, function(err, data) {
+
+//     console.log(JSON.stringify(data, null, 2));
+//   });
+
+// });
